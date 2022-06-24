db8759b340ca4988354b7f6cc2f51503cacbe8e4
dire akanbi <direakanbi@icloud.com>
2022-06-24 09:58:20 +0100

    Fix Linter Errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
- 
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # get 'shopping/list'
-  get "/general_shopping_list", to: 'shoppings#list'
+  get '/general_shopping_list', to: 'shoppings#list'
   resources :publics, only: %i[index]
   resources :recipes, only: %i[index new create destroy show]
   resources :foods, only: %i[index new create destroy show]
